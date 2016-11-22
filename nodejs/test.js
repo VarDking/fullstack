4b9561fa459423fceb143e00fccee825039c53d7
VarDking <dkingcom@163.com>
2016-11-22 14:15:15 +0800

    test

--- nodejs/test.js
+++ nodejs/test.js
@@ -13,22 +13,23 @@ module.exports = {
 
 function deptClockData(deptid, start, end) {
     return request.postAsync({
-            url : 'http://10.32.64.26:8011/Card.aspx',
-            json: true,
-            body: {
-                act     : '804',
-                startime: start,
-                endtime : end,
-                deptid  : String(deptid),
-                portkey : 'c2f65e2bb6f74b3bbb2c163485b27d37'
-            }
-        }).then(result=> {
-            if (result.body && result.body.code) {
-        return result.body.message;
-    } else {
-        return Promise.reject('请求运维接口失败！');
-    }
-});
+        url : 'http://10.32.64.26:8011/Card.aspx',
+        json: true,
+        body: {
+            act     : '804',
+            startime: start,
+            endtime : end,
+            deptid  : String(deptid),
+            portkey : 'c2f65e2bb6f74b3bbb2c163485b27d37'
+        }
+    }).then(result=> {
+        console.log('result.body',result.body);
+        if (result.body && result.body.code) {
+            return result.body.message;
+        } else {
+            return Promise.reject('请求运维接口失败！');
+        }
+    });
 }
 
-deptClockData('1500406','2016-11-21','2016-11-21').then(console.log);
+deptClockData('1500406', '2016-11-21', '2016-11-21').then(console.log);
