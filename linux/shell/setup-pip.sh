9a0f960b2407edbc1faff7fc511be85c90ade422
vardking <vardking@gmail.com>
2017-09-06 23:31:34 +0800

    简单安装pip

--- linux/shell/setup-pip.sh
+++ linux/shell/setup-pip.sh
@@ -7,5 +7,5 @@ if ! command -v python > /dev/null 2>&1; then
 	apt-get install python
 fi
 
-wget https://bootstrap.pypa.io/get-pip.py && python get-pip.py
+wget "https://bootstrap.pypa.io/get-pip.py" && python get-pip.py
 rm get-pip.py
