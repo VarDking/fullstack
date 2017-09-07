aa477e63f5a92a5f41f630477bb62ecdbfe1ce99
vardking <vardking@gmail.com>
2017-09-08 02:30:14 +0800

    移除软链接

--- linux/shell/graphicsmagick.sh
+++ linux/shell/graphicsmagick.sh
@@ -24,10 +24,8 @@ mkdir ${tmp_path} && cd ${tmp_path}
 wget http://downloads.webmproject.org/releases/webp/libwebp-0.4.3.tar.gz
 tar -xvzf libwebp-0.4.3.tar.gz
 cd libwebp-0.4.3
-./configure --prefix=/usr/local/libwebp-0.4.3
+./configure --prefix=/usr/local/libwebp
 make && make install
-cd /usr/local
-sudo ln -s libwebp-0.4.3 libwebp
 cd ${tmp_path}
 
 # 支持jpeg格式
@@ -36,28 +34,22 @@ tar -xzvf jpegsrc.v9a.tar.gz
 cd jpeg-9a
 ./configure --prefix=/usr/local/libjpeg-9a
 make && make install
-cd /usr/local
-ln -s libjpeg-9a /usr/local/libjpeg
 cd ${tmp_path}
 
 # 支持png格式
 wget http://download.sourceforge.net/libpng/libpng-1.6.18.tar.gz
 tar -xzvf libpng-1.6.18.tar.gz
 cd libpng-1.6.18
-./configure --prefix=/usr/local/libpng-1.6.18
+./configure --prefix=/usr/local/libpng
 make && make install
-cd /usr/local
-ln -s libpng-1.6.18 /usr/local/libpng
 cd ${tmp_path}
 
 # 支持 tiff
 wget http://download.osgeo.org/libtiff/tiff-4.0.4.tar.gz
 tar -xzvf tiff-4.0.4.tar.gz
 cd tiff-4.0.4
-./configure --prefix=/usr/local/libtiff-4.0.4
+./configure --prefix=/usr/local/libtiff
 make && make install
-cd /usr/local
-ln -s libtiff-4.0.4 /usr/local/libtiff
 cd ${tmp_path}
 
 # 安装GraphicsMagick
@@ -69,10 +61,10 @@ cd GraphicsMagick-1.3.22
   CPPFLAGS="-I/usr/local/libjpeg/include -I/usr/local/libpng/include -I/usr/local/libtiff/include"
 
 make && make install && make check
-ln -s graphicsmagick-1.3.22 /usr/local/graphicsmagick
 cd ${tmp_path}/../
 
 # 重载动态链接库
 ldconfig
+
 rm -r ${tmp_path}
 echo "安装成功"
