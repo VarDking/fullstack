20a32c926318c7e212c7fb90d88ed1371400ca33
vardking <vardking@gmail.com>
2017-09-07 15:09:00 +0800

    add python-dev

--- linux/shell/setup-base-deps-debian.sh
+++ linux/shell/setup-base-deps-debian.sh
@@ -2,4 +2,4 @@
 
 #基本开发环境
 echo "installing git, wget, curl, build-essential, rsync, pkg-config, python..."
-bash -c "apt-get install -qq -y git wget curl build-essential libssl-dev rsync pkg-config python < /dev/null" > /dev/null 2>/dev/null
+bash -c "apt-get install -qq -y git wget curl build-essential libssl-dev rsync pkg-config python python-dev< /dev/null" > /dev/null 2>/dev/null
