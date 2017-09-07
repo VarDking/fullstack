1cb6e0ceb4c7648af0839118325d3138ba2062c5
vardking <vardking@gmail.com>
2017-09-07 10:48:35 +0800

    安装pip root权限

--- linux/shell/pip.sh
+++ linux/shell/pip.sh
@@ -3,6 +3,12 @@
 
 set -e
 
+# root 权限
+if [ "$(id -u)" != "0" ]; then
+echo "This script must be run as root" 1>&2
+exit 1
+fi
+
 if ! command -v python > /dev/null 2>&1; then
 	apt-get install python
 fi
