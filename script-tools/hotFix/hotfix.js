bb955735f94d1ae6091ea7bd5f4a6f1e5ddfeccd
vardking <vardking@gmail.com>
2017-12-12 18:23:48 +0800

    调整热更新脚本

--- script-tools/hotFix/hotfix.js
+++ script-tools/hotFix/hotfix.js
@@ -3,19 +3,30 @@
  * 热更新补丁包生成脚本
  */
 'use strict';
-var fs         = require('fs');
-var walk       = require('walk');
-var path       = require('path');
-var hasha      = require('hasha');
-var async      = require('async');
-var archiver   = require('archiver');
-var underscore = require('underscore');
-var baseName   = path.basename;
+const fs       = require('fs');
+const path     = require('path');
+const _        = require('lodash');
+const chalk    = require('chalk');
+const shell    = require('shelljs');
+const baseName = path.basename;
 
-var cmdArgs           = process.argv.splice(2);
-var hotFixResourceDir = cmdArgs[0] || '/var/www/mj/hotFix';
-var hotFixZipDir      = cmdArgs[1] || './public/hotFix';
+const cmdArgs           = process.argv.splice(2);
+const hotFixResourceDir = cmdArgs[0] || '/var/www/mj/hotFix';
+const hotFixZipDir      = cmdArgs[1] || './public/hotFix';
 
+class Logger {
+    static info(str) {
+        console.log(chalk.green(str));
+    }
+
+    static error(str) {
+        console.log(chalk.red(str));
+    }
+
+    static process(str) {
+        console.log(chalk.blue(str));
+    }
+}
 
 /**
  * 列举文件夹目录
@@ -32,10 +43,10 @@ function listDir(dir) {
  * @param versionList
  */
 function groupVersionList(versionList) {
-    var versionGroup = underscore.groupBy(versionList, function (version) {
+    let versionGroup = _.groupBy(versionList, function (version) {
         return version.split('.').slice(0, 2);
     });
-    return underscore.map(versionGroup, function (group) {
+    return _.map(versionGroup, function (group) {
         return group.sort(function (a, b) {
             return needUpdate(a, b) ? -1 : 1;
         });
@@ -48,33 +59,22 @@ function groupVersionList(versionList) {
  * @param newVersion
  */
 function needUpdate(oldVersion, newVersion) {
-    if ( oldVersion === newVersion ) {
+    if (oldVersion === newVersion) {
         return false;
     }
 
     oldVersion = oldVersion.split('.');
     newVersion = newVersion.split('.');
 
-    var len = Math.min(oldVersion.length, newVersion.length);
+    let len = Math.min(oldVersion.length, newVersion.length);
 
-    for (var i = 0; i < len; i++) {
-        if ( +oldVersion[i] < +newVersion[i] ) {
+    for (let i = 0; i < len; i++) {
+        if (+oldVersion[i] < +newVersion[i]) {
             return true;
         }
     }
 
     return oldVersion.length < newVersion.length;
-}
-
-/**
- * 计算文件的md5值
- * @param filePath
- * @param cb
- */
-function fileMd5(filePath, cb) {
-    hasha.fromFile(filePath, {algorithm: 'md5'}).then(function (hashNum) {
-        return cb(null, hashNum);
-    }).catch(cb);
 }
 
 /**
@@ -92,102 +92,72 @@ function fileExistSync(filePath) {
 }
 
 /**
- * 简单判断文件是否存在(异步)
- * @param filePath
- * @param cb
- */
-function fileExist(filePath, cb) {
-    fs.access(filePath, function (err) {
-        err ? cb(null, false) : cb(null, true);
-    });
-}
-
-/**
  * 比较文件夹,并生成补丁压缩包
  * @param oldFolder
  * @param newFolder
+ * @param newestVersion
  * @param platform
- * @param cb
  */
-function createHotFixZip(oldFolder, newFolder, platform, cb) {
-    var updateStr   = [baseName(oldFolder), baseName(newFolder)].join('to');
-    var zipFileName = updateStr + '.zip';
-    // var zipFileName = hasha(updateStr, {algorithm : 'md5'}) + '.zip';
+function createHotFixZip(oldFolder, newFolder, newestVersion, platform) {
+    let destPath    = `${hotFixZipDir}/${newestVersion}/${platform}`;
+    let updateStr   = [baseName(oldFolder), baseName(newFolder)].join('to');
+    let destPackage = `${destPath}/${updateStr}.zip`;
+    let tmpDir      = `${destPath}/_tmp/`;
 
-    if ( fileExistSync(zipFileName) ) {
-        return cb(null, zipFileName);
+    if (fileExistSync(destPackage)) {
+        shell.exec(`rm ${destPackage}`);
     }
 
-    var outPut  = fs.createWriteStream(path.join(hotFixZipDir, platform, zipFileName));
-    var archive = archiver('zip', {store: true});
-    archive.pipe(outPut);
+    if (fileExistSync(tmpDir)) {
+        shell.exec(`rm -r ${tmpDir}`);
+    }
 
-    walk
-        .walk(newFolder, {followLinks: false})
-        .on('file', function (root, stat, next) {
-            var filePath    = path.join(root, stat.name).substr(newFolder.length);
-            var newFilePath = path.join(newFolder, filePath);
-            var oldFilePath = path.join(oldFolder, filePath);
+    shell.exec(`mkdir -p ${tmpDir}`);
 
-            fileExist(oldFilePath, function (err, hasFile) {
-                if ( !hasFile ) {
-                    archive.file(newFilePath, {name: newFilePath.substr(newFolder.length)});
-                    return next();
-                }
-                async.map(
-                    [newFilePath, oldFilePath],
-                    fileMd5,
-                    function (err, results) {
-                        if ( err ) {
-                            return next(err);
-                        }
-                        if ( results[0] !== results[1] ) {
-                            archive.file(newFilePath, {name: newFilePath.substr(newFolder.length)});
-                        }
-                        next();
-                    });
-            });
-        })
-        .on('end', function () {
-            archive.finalize();
-            cb(null, zipFileName);
-        })
-        .on('error', cb)
+    let deltaCmd = `rsync -rc --compare-dest=${oldFolder}/ ${newFolder}/ ${tmpDir}`;
+    let zipCmd   = `cd ${tmpDir} && zip -qr ${destPackage} .`;
+
+    shell.exec(deltaCmd);
+
+    Logger.info(`生成压缩包 ${platform} ${updateStr} ...`);
+    shell.exec(zipCmd);
+
+    shell.exec(`rm -r ${tmpDir}`);
+    return true;
 }
 
 /**
  * 生成热更新包
  */
-function zip(hotFixResourceDir, platform, cb) {
-    console.log('start scanning ' + hotFixResourceDir + ' ...... ');
+function zip(hotFixResourceDir, platform) {
+    Logger.info('start scanning ' + hotFixResourceDir + ' ...... ');
 
-    async.each(groupVersionList(listDir(hotFixResourceDir)), function (versionList, cb) {
-        var newestVersion = versionList[versionList.length - 1];
-        async.each(versionList, function (version, cb) {
-            if ( version === newestVersion ) {
-                return cb(null);
+    let groupVersion = groupVersionList(listDir(hotFixResourceDir));
+    groupVersion.map(versionList => {
+        let newestVersion = _.last(versionList);
+        versionList.map(version => {
+            if (version === newestVersion) {
+                return true;
             }
-            var oldFoldPath = path.join(hotFixResourceDir, version);
-            var newFoldPath = path.join(hotFixResourceDir, newestVersion);
-
-            console.log('creating zip, from', oldFoldPath + ' => ' + newFoldPath);
-            createHotFixZip(oldFoldPath, newFoldPath, platform, cb);
-        }, cb);
-    }, cb);
+            let oldFoldPath = path.join(hotFixResourceDir, version);
+            let newFoldPath = path.join(hotFixResourceDir, newestVersion);
+            if (!fileExistSync(newFoldPath)) {
+                shell.exec(`mkdir -p ${newFoldPath}`);
+            }
+            createHotFixZip(oldFoldPath, newFoldPath, newestVersion, platform);
+        });
+    });
 
 }
 
 function start() {
-    console.log('start creating ...');
-    async.each([hotFixResourceDir + '/ios', hotFixResourceDir + '/android'], function (path, cb) {
-        var platform = path.substring(path.lastIndexOf('/')+1);
-        return zip(path, platform, cb);
-    }, function (err) {
-        if ( err ) {
-            return console.error(err);
-        }
-        console.log('success, finished!')
+    Logger.info('start creating ...');
+
+    ['android', 'ios'].map(platform => {
+        return zip(hotFixResourceDir + '/' + platform, platform);
     });
+
+    Logger.info('打包成功！');
 }
 
 start();
--- script-tools/hotFix/hotfix.sh
+++ script-tools/hotFix/hotfix.sh
@@ -1,19 +1,19 @@
 # !/bin/sh
-# 热更新生成脚本
+# 从svn生成热更新包
 # $1 地区 $2 svn版本号 $3 版本号
 # 使用示例 bash hotFix.sh shandong 1005 1.2.25
 
 set -e
 
 if [ ! $1 ] || [ ! $2 ] || [ ! $3 ]; then
-	echo "参数错误！ 使用示例 bash hotFix.sh [地区名] [svn版本号] [游戏版本号]" 1>&2
-	exit 1
+  echo "参数错误！ 使用示例 bash hotFix.sh [地区名] [svn版本号] [游戏版本号]" 1>&2
+  exit 1
 fi
 
-assetsPath=/data/services/majiang/StreamingAssets;
+assetsPath=/data/services/majiang/StreamingAssets/Assets/StreamingAssets;
 updateAssetsPath=/data/services/majiang/update;
-majiangPath=/data/services/majiang/server/trunk;
-hotfixPackageDestPath=${majiangPath}/web-server/public/hotFix/$1;
+majiangPath=/data/services/majiang;
+hotfixPackageDestPath=/data/samba/share/hotfix/$1;
 
 # 确保文件夹存在
 [ ! -d "${updateAssetsPath}" ] && mkdir -p ${updateAssetsPath};
@@ -22,16 +22,12 @@ hotfixPackageDestPath=${majiangPath}/web-server/public/hotFix/$1;
 [ ! -d "${hotfixPackageDestPath}/ios" ] && mkdir -p ${hotfixPackageDestPath}/ios;
 
 # 从svn 检出 StreamingAssets
-if [ $2 ] && [ $2 -gt 0 ]; then
+if [ $2 ] ; then
    cd ${assetsPath};
-   svn up -r $2;
-   find . -type f -name "*.meta" -exec rm -rf {} \;
+#   svn up -r $2;
+   git pull origin master
+   git checkout $2 .
 fi
-
-# 创建地区目录
-cd ${majiangPath}/web-server/public/hotFix
-mkdir -p $1/android
-mkdir -p $1/ios
 
 # 复制资源文件
 cd ${updateAssetsPath}
@@ -56,35 +52,13 @@ cp -fRp config ${updateAssetsPath}/$1/ios/$3/
 cp -fRp Lua_1 ${updateAssetsPath}/$1/ios/$3/
 cp -fRp iOS/AssetBundles ${updateAssetsPath}/$1/ios/$3/
 
-
 # 清理文件
 cd ${updateAssetsPath}/$1
 find . -type f -name ".DS_Store" -exec rm -rf {} \;
 find . -type d -name ".svn" -prune -exec rm -rf {} \;
+find . -type f -name "*.meta" -exec rm -rf {} \;
 
 cd ${majiangPath}/web-server
 node hotfix.js ${updateAssetsPath}/$1 ${hotfixPackageDestPath}
 
-# 修复zip文件格式
-function rezip(){
- mkdir tmp
- for f in *zip; do
-     cd tmp
-     rm -rf *
-     unzip ../"$f"
-     rm -r ../"$f"
-     zip -r ../"$f" *
-     rm -rf *
-     cd ..
- done
- rm -r tmp
-}
-
-cd ${hotfixPackageDestPath}/ios
-rezip;
-
-cd ${hotfixPackageDestPath}/android
-rezip;
-
 echo "热更新文件生成成功！";
-
